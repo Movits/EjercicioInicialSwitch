ce99f51fd55582235881cb30f7d7904ec1ecf2b2
Roberto y Leandro <leanbazan99@gmail.com>
2023-05-04 11:32:31 -0300

    agregamos color

--- scripts.js
+++ scripts.js
@@ -2,13 +2,26 @@ const pagina = document.querySelector("body");
 const selectOpcionesColor = document.querySelector("select");
 
 function cambiarColorTexto(){
-    switch (parseInt(selectOpcionesColor)) {
+    switch (parseInt(selectOpcionesColor.value)) {
         case 1:
+            console.log("color blue");
             pagina.style.color="blue";
-            pagina.style.backgroundColor="cream";
+            pagina.style.backgroundColor="yellow";
             break;
-    
+        case 2:
+            console.log("color yellow");
+            pagina.style.color="yellow";
+            pagina.style.backgroundColor="blue";
+            break;
+        case 3:
+            console.log("color green");
+            pagina.style.color="green";
+            pagina.style.backgroundColor="yellow";
+            break;
         default:
+            console.log("default");
+            pagina.style.color="black";
+            pagina.style.backgroundColor="white";
             break;
     }
 }
